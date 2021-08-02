527d6b63e876b5fe71a8db8d781b131359569a68
kushagra rathore <kushagrarathore002@gmail.com>
2021-08-03 01:48:23 +0530

    Formated the code

--- src/App.js
+++ src/App.js
@@ -1,127 +1,94 @@
-import React, { Component } from 'react';
-import Styles from './App.module.css';
-import Amain from './components/Amain';
-import Ls from './components/Ls';
-import cross from './cross.png';
-class App extends Component{
-    
-    state={
-        show:true,
-        com:false,
-        all:true,ourInt:false
-        
-        
-        
-    }
-    lsHandler=()=>
-    {
-        this.setState({show:!this.state.show,ourInt:!this.state.ourInt});
-        
+import React, { Component } from "react";
+import Styles from "./App.module.css";
+import Amain from "./components/Amain";
+import Ls from "./components/Ls";
+import cross from "./cross.png";
+class App extends Component {
+  state = {
+    show: true,
+    com: false,
+    all: true,
+    ourInt: false,
+  };
+  lsHandler = () => {
+    this.setState({ show: !this.state.show, ourInt: !this.state.ourInt });
+  };
+  comHandeler = () => {
+    this.setState({ com: true });
+    setTimeout(() => {
+      this.lsHandler();
+    }, 500);
+  };
+  unComHandler = () => {
+    this.setState({ com: false });
+    this.setState({ all: false });
+    setTimeout(() => {
+      this.lsHandler();
+    }, 500);
+  };
+  allHandler = () => {
+    this.setState({ all: true });
+    setTimeout(() => {
+      this.lsHandler();
+    }, 500);
+  };
 
-    }
-    comHandeler=()=>
-    {
-        this.setState({com:true});
-        setTimeout(()=>
-        {
-            this.lsHandler();
-        },500);
-    }
-    unComHandler=()=>
-    {
-        this.setState({com:false});
-        this.setState({all:false});
-        setTimeout(()=>
-        {
-            this.lsHandler();
-        },500)
-    }
-    allHandler=()=>
-    {
-        this.setState({all:true});
-        setTimeout(()=>
-        {
-            this.lsHandler();
-        },500);
-    }
-    
-    hand=()=>
-    {
-        
-        if(this.state.show)
-        {
-
-        
-        return(
-            <div  className={Styles.mainS} >
-    
-            <Amain com={this.state.com} all={this.state.all} ourInt={this.state.ourInt}/>
-            <Ls lsHandler={this.lsHandler} />
+  hand = () => {
+    if (this.state.show) {
+      return (
+        <div className={Styles.mainS}>
+          <Amain
+            com={this.state.com}
+            all={this.state.all}
+            ourInt={this.state.ourInt}
+          />
+          <Ls lsHandler={this.lsHandler} />
         </div>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <div className={Styles.lis}>
+            <button className={Styles.crossbtn} onClick={this.lsHandler}>
+              <img className={Styles.cross} src={cross} alt="Not Found" />
+            </button>
 
-        )
-        }
-        else
-        {
-            return(
-                <React.Fragment>
-                   
-                
-                <div className={Styles.lis}>
-                    <button className={Styles.crossbtn} onClick={this.lsHandler}>
-                    <img  className={Styles.cross} src={cross} alt="Not Found"/>
-                    </button>
-                        
-                <ul>
-                    <li >
-                        <button onClick={this.allHandler}>
-                            <strong>All</strong>
-                        </button>
-                    </li>
+            <ul>
+              <li>
+                <button onClick={this.allHandler}>
+                  <strong>All</strong>
+                </button>
+              </li>
 
-                    <li>
-                        <button onClick={this.comHandeler}>
-                            <strong>Completed</strong>
-                        </button>
-                    </li>
-                    
-                    <li>
-                        <button onClick={this.unComHandler}>
-                            <strong>Uncompleted</strong>
-                        </button>
-                    </li>
-                </ul>
-                </div>
-                
-                <div  className={Styles.mainH} > 
-                
-                <Amain com={this.state.com} all={this.state.all} ourInt={this.state.ourInt}/>
-              
-                <Ls lsHandler={this.lsHandler} />
-                </div>
-                
+              <li>
+                <button onClick={this.comHandeler}>
+                  <strong>Completed</strong>
+                </button>
+              </li>
 
-</React.Fragment>
+              <li>
+                <button onClick={this.unComHandler}>
+                  <strong>Uncompleted</strong>
+                </button>
+              </li>
+            </ul>
+          </div>
 
-            )
-        }
+          <div className={Styles.mainH}>
+            <Amain
+              com={this.state.com}
+              all={this.state.all}
+              ourInt={this.state.ourInt}
+            />
+
+            <Ls lsHandler={this.lsHandler} />
+          </div>
+        </React.Fragment>
+      );
     }
-    render()
-    {
-        return(
-            <>
-            
-            {this.hand()}
-                
-            
-               
-               
-
-                    
-           
-
-            </>
-        )
-    }
+  };
+  render() {
+    return <>{this.hand()}</>;
+  }
 }
 export default App;
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,215 +1,171 @@
-import React from 'react';
-import List from './List';
+import React from "react";
+import List from "./List";
 import Search from "./Search";
-import styles from './Student.module.css';
-function Student({setName,name,nameis,setNameDis,deleteit,checkedHandler,searchbtnhandler,Fname,fBool,com,all})
-{
-    if(checkedHandler==='')
-    {
-        return(
-            <div className={styles.initial} style={{pointerEvents:"none"}}>
-            <div className={styles.intext}>
-             <input type="text" className={styles.input} onChange={setName} value={name} />
-             <button className={styles.clickadd} onClick={setNameDis}>Add</button>
-             
-                 </div>
-             <Search searchbtnhandler={searchbtnhandler}/> 
-             </div>
-                 );
-    }
-    
-         else if(!fBool)
-            {
-                return(
-                    <div className={styles.initial}>
-                    
-            
-                   <div className={styles.intext}>
-                    <input type="text" className={styles.input} onChange={setName} value={name} />
-                    <button className={styles.clickadd} onClick={setNameDis}>Add</button>
-                        </div> 
-                    <Search searchbtnhandler={searchbtnhandler}/>
-                    { comhand(com,all)
-                    
-                       
-                    
-            }
-
-                            
-                    
-                    {
-                    nameis.map((indi,i) =>
-                        {
-                            if(com===true)
-                            {
-                                if(indi.checked===true)
-                                {
-
-                                    
-                                    
-                                    
-                                    
-                                    return(
-                                        <>
-
-                                        <List key={i} checked={indi.checked} deleteit={deleteit} id={indi.id} indiname={indi.kName} checkedHandler={checkedHandler}  />
-                                        </>
-                                        )
-                                    }
-                                    else{
-                                        return null;
-                                    }
-                                        
-                            }
-                            
-                                else if(all===true)
-                                {
-                                    
-                                    return(
-                                        <List key={i} checked={indi.checked} deleteit={deleteit} id={indi.id} indiname={indi.kName} checkedHandler={checkedHandler} />
-                                        
-                                        
-                                        //  <h1 key={i}>  student : {n}</h1>
-                                        
-                                        
-                                        
-                                        
-                                        )
-
-                                }
-                                
-                            else if(com===false)
-                            {
-                                if(indi.checked===false)
-                                {
-
-                                    
-                                    
-                                    return(
-                                        <List key={i} checked={indi.checked} deleteit={deleteit} id={indi.id} indiname={indi.kName} checkedHandler={checkedHandler}  />
-                                        
-                                        
-                                        //  <h1 key={i}>  student : {n}</h1>
-                                        
-                                        
-                                        
-                                        
-                                        )
-                                    }
-                                    else{
-                                        return null;
-                                    }
-                                }
-                                return null;
-                        
-                        })
-
-                        }
-                      
-                    
-
+import styles from "./Student.module.css";
+function Student({
+  setName,
+  name,
+  nameis,
+  setNameDis,
+  deleteit,
+  checkedHandler,
+  searchbtnhandler,
+  Fname,
+  fBool,
+  com,
+  all,
+}) {
+  if (checkedHandler === "") {
+    return (
+      <div className={styles.initial} style={{ pointerEvents: "none" }}>
+        <div className={styles.intext}>
+          <input
+            type="text"
+            className={styles.input}
+            onChange={setName}
+            value={name}
+          />
+          <button className={styles.clickadd} onClick={setNameDis}>
+            Add
+          </button>
         </div>
-        
-
-                )
-
-            }
-
-
-
-            if(fBool)
-            {
-                return(
-                    <div className={styles.initial}>
-                    
-                    <Search searchbtnhandler={searchbtnhandler}/>
-                    {
-                    nameis.map((indi,i) =>
-                        {
-                            if(indi.kName.toLowerCase().indexOf(Fname.toLowerCase())!==-1)
-                            {
-
-                                
-                                return(
-                                    <div key={indi.id} style={{backgroundColor:"white"}}>
-                                    <List key={indi.id} deleteit={deleteit} id={indi.id} indiname={indi.kName} checkedHandler={checkedHandler} checked={indi.checked} />
-                                        </div>
-                                    //  <h1 key={i}>  student : {n}</h1>
-                                    )
-                                }
-                                return null;
-                                
-                                
-                            
-                            
-                                
-                                
-                                
-                                
-                        
-                        })
-                    }
-                    
-            
-
-                            
-                                
-                                <div style={{position:"absolute",left:"32%",top:"90px",zIndex:"-100"}}>
-                                <h2>
-                                
-                                </h2>
-                                </div>
-                            
-                        
-                        
-
-                        
-                        
-            
-                      
-                    
-
+        <Search searchbtnhandler={searchbtnhandler} />
+      </div>
+    );
+  } else if (!fBool) {
+    return (
+      <div className={styles.initial}>
+        <div className={styles.intext}>
+          <input
+            type="text"
+            className={styles.input}
+            onChange={setName}
+            value={name}
+          />
+          <button className={styles.clickadd} onClick={setNameDis}>
+            Add
+          </button>
         </div>
-        
+        <Search searchbtnhandler={searchbtnhandler} />
+        {comhand(com, all)}
 
-                )
-
+        {nameis.map((indi, i) => {
+          if (com === true) {
+            if (indi.checked === true) {
+              return (
+                <>
+                  <List
+                    key={i}
+                    checked={indi.checked}
+                    deleteit={deleteit}
+                    id={indi.id}
+                    indiname={indi.kName}
+                    checkedHandler={checkedHandler}
+                  />
+                </>
+              );
+            } else {
+              return null;
             }
-        
-        
-         
+          } else if (all === true) {
+            return (
+              <List
+                key={i}
+                checked={indi.checked}
+                deleteit={deleteit}
+                id={indi.id}
+                indiname={indi.kName}
+                checkedHandler={checkedHandler}
+              />
+
+              //  <h1 key={i}>  student : {n}</h1>
+            );
+          } else if (com === false) {
+            if (indi.checked === false) {
+              return (
+                <List
+                  key={i}
+                  checked={indi.checked}
+                  deleteit={deleteit}
+                  id={indi.id}
+                  indiname={indi.kName}
+                  checkedHandler={checkedHandler}
+                />
+
+                //  <h1 key={i}>  student : {n}</h1>
+              );
+            } else {
+              return null;
+            }
+          }
+          return null;
+        })}
+      </div>
+    );
+  }
+
+  if (fBool) {
+    return (
+      <div className={styles.initial}>
+        <Search searchbtnhandler={searchbtnhandler} />
+        {nameis.map((indi, i) => {
+          if (indi.kName.toLowerCase().indexOf(Fname.toLowerCase()) !== -1) {
+            return (
+              <div key={indi.id} style={{ backgroundColor: "white" }}>
+                <List
+                  key={indi.id}
+                  deleteit={deleteit}
+                  id={indi.id}
+                  indiname={indi.kName}
+                  checkedHandler={checkedHandler}
+                  checked={indi.checked}
+                />
+              </div>
+              //  <h1 key={i}>  student : {n}</h1>
+            );
+          }
+          return null;
+        })}
+
+        <div
+          style={{
+            position: "absolute",
+            left: "32%",
+            top: "90px",
+            zIndex: "-100",
+          }}
+        >
+          <h2></h2>
+        </div>
+      </div>
+    );
+  }
 }
-function comhand(com,all)
-{
-    if(com)
-    {
-        return(
-            <div className={styles.com}>
-             <h3>
-             <strong>Completed</strong>
-             </h3>
-            </div>
-        )
-    }
-    else if(all){
-        return (
-            <div className={styles.com}>
-                <h3>
-                    <strong>All</strong>
-                </h3>
-            </div>
-        )
-    }
-    else if(!com)
-    {
-        return(
-            <div className={styles.com} >
-                <h3>
-                    <strong>Uncompleted</strong>
-                </h3>
-            </div>
-        )
-    }
-    
+function comhand(com, all) {
+  if (com) {
+    return (
+      <div className={styles.com}>
+        <h3>
+          <strong>Completed</strong>
+        </h3>
+      </div>
+    );
+  } else if (all) {
+    return (
+      <div className={styles.com}>
+        <h3>
+          <strong>All</strong>
+        </h3>
+      </div>
+    );
+  } else if (!com) {
+    return (
+      <div className={styles.com}>
+        <h3>
+          <strong>Uncompleted</strong>
+        </h3>
+      </div>
+    );
+  }
 }
 export default Student;
